49ee4754622d1292a3fcf612f7d02cd6d824afdc
neeraj <neerajkomatishetti@gmail.com>
2025-02-14 21:11:27 +0530

    code edit in first_neuron

--- first_neuron_date_13022025.py
+++ first_neuron_date_13022025.py
@@ -1,7 +1,13 @@
+def neuron(inputs, weights, bias=0.0, activation=lambda x: x):
+    z = sum(x * w for x, w in zip(inputs, weights)) + bias
+    return activation(z) #Calculates the neuron's output with an optional activation function.
 
-
-def neuron(list: X , list: weight):
-    return sum(X[i] * weight[i] for i in range(len(X)))
-
+# Example input
 inputs = [1, 2, 3, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2
+
+# Neuron output using the identity activation function
+output = neuron(inputs, weights, bias)
+print("Neuron output:", output)
 
